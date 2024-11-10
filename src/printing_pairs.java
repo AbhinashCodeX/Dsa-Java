c694a267000796421cbcc89a9cf09a9d550bf302
Abhinash Panigrahi <abhinashpanigrahi9@gmail.com>
2024-11-10 12:58:52 +0530

    Printing Pairs

---
+++ src/printing_pairs.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class printing_pairs {
+    static void printingpairs(int[] arr, int n){
+
+        for(int i=0;i<n;i++){
+            for(int j=i; j<n;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k] + ",");
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
+
+// Print the sum of each subarray & find out the largest sum
+
+
+    public static void main(String[] args){
+        //Array Containing
+
+        int arr[] = {-2,3,4,5,-12,6,2,3};
+        int n = arr.length;
+
+        printingpairs(arr,n);
+
+    }
+
+}
