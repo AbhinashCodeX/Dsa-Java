abc1efd990cd53ce6b047627cbd4e3c29d7a24e8
Abhinash Panigrahi <abhinashpanigrahi9@gmail.com>
2024-11-10 12:06:40 +0530

    Practiced the binary search

--- src/Binary_search.java
+++ src/Binary_search.java
@@ -1,22 +1,40 @@
 import java.util.*;
 public class Binary_search {
-    static int binarysearch(int[] arr,int n , int key){
+    //    static int binarysearch(int[] arr,int n , int key){
+//        int s = 0;
+//        int e = n-1;
+//
+//        while(s<=e){
+//            int mid = (s+e)/2;
+//            if(arr[mid] == key){
+//                return mid;
+//            }else if(arr[mid]> key){
+//                e = mid -1;
+//            }else{
+//                s = mid +1;
+//            }
+//        }
+//        return -1;
+//
+//    }
+    static int binarysearch(int[] arr, int n, int key) {
         int s = 0;
-        int e = n-1;
+        int e = n - 1;
 
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(arr[mid] == key){
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] == key) {
                 return mid;
-            }else if(arr[mid]> key){
-                e = mid -1;
-            }else{
-                s = mid +1;
+            } else if (arr[mid] > key) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
         return -1;
-
     }
+
+
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
@@ -34,3 +52,4 @@ public class Binary_search {
         }
     }
 }
+
